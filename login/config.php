05eda6ca8bca2183e10653313e7119ea3fe317ae
redpois0n <red@cmail.nu>
2015-04-27 22:00:10 +0200

    config.php

---
+++ login/config.php
@@ -0,0 +1,3 @@
+<?php
+
+const MAPFILE = "../map.txt";
--- login/login.php
+++ login/login.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once "config.php";
+
 if (isset($_POST['username']) && isset($_POST['password'])) {
 	$login = $_POST['username'];
 	$pass = $_POST['password'];
