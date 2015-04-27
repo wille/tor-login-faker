b2368379d5476caba29048fcebbbc929cdafc1fd
redpois0n <red@cmail.nu>
2015-04-27 15:46:25 +0200

    Work on send script, __init__.py

--- send.py
+++ send.py
@@ -1,4 +1,16 @@
+import socket
+import requests
+import socks
+import stem.process
+import hashlib
+import os
+import scraper
 
+def send():
+    fingerprints = scraper.scrapeNodes()
+    
+    for fp in fingerprints:
+        pass
 
 if __name__ == "__main__":
-    pass
+    send()
