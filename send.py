32f3ac99ef89789bf00d62eec36752ef7aefb759
redpois0n <red@cmail.nu>
2015-04-27 22:05:40 +0200

    Some work, only works once then hangs. posts data to PANEL_URL, static user and pass

--- send.py
+++ send.py
@@ -6,11 +6,37 @@ import hashlib
 import os
 import scraper
 
+PANEL_URL = "http://localhost/login.php"
+PORT = 1000
+
 def send():
     fingerprints = scraper.scrapeNodes()
     
     for fp in fingerprints:
-        pass
+        global tor_process
+        print("Testing " + fp)
+        
+        try:
+            tor_process = stem.process.launch_tor_with_config(
+                config = {
+                    'SocksPort': str(PORT),
+                    'ExitNodes': fp,
+                },
+            )
+                        
+            data = {
+                "user": "admin",
+                "password": "login"
+            }
+            
+            requests.get(PANEL_URL, data=data, timeout=10)
+            
+            print("Completed " + fp)
+            
+            tor_process.kill()
+        except Exception as e: 
+            print(str(e))
+            tor_process.kill()
 
 if __name__ == "__main__":
     send()
