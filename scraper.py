0af92c2fcdb9e1a56fbf33ecad115ae819233d4d
redpois0n <red@cmail.nu>
2015-04-27 15:04:59 +0200

    fingerprint scaper works

--- scraper.py
+++ scraper.py
@@ -1,5 +1,17 @@
-def scrapeNodes(amount=-1):
-    pass
+import requests
 
+def scrapeNodes(amount=-1):
+    r = requests.get("https://check.torproject.org/exit-addresses", timeout=15)
+
+    fingerprints = []
+    
+    for l in r.content.split("\n"):
+        if l.startswith("ExitNode "):
+            l = l[10:]
+            print("Found node " + l)
+            fingerprints.append(l)
+            
+    return fingerprints
+        
 if __name__ == "__main__":
     scrapeNodes()
