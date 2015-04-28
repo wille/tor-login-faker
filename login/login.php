9ca1a9b6a5faf807b2c1061d37884f6a60fdf36b
redpois0n <red@cmail.nu>
2015-04-28 13:46:29 +0200

    Parses map file

--- login/login.php
+++ login/login.php
@@ -8,7 +8,12 @@ if (RUNNING) {
 		$pass = $_POST['password'];
 	
 		foreach (file(MAPFILE) as $line) {
-	
+			if (substr($line, 0, 1) != "#") {
+				$raw = explode("=", trim($line));
+			
+				$fingerprint = $raw[0];
+				$password = $raw[1];
+			}
 		}
 	}
 }
