37585aa876550eb82f3c6332d02f5ab2b1ed0631
redpois0n <red@cmail.nu>
2015-04-28 13:49:14 +0200

    detects matching password, warns

--- login/login.php
+++ login/login.php
@@ -5,7 +5,7 @@ require_once "config.php";
 if (RUNNING) {
 	if (isset($_POST['username']) && isset($_POST['password'])) {
 		$login = $_POST['username'];
-		$pass = $_POST['password'];
+		$postpass = $_POST['password'];
 	
 		foreach (file(MAPFILE) as $line) {
 			if (substr($line, 0, 1) != "#") {
@@ -13,6 +13,11 @@ if (RUNNING) {
 			
 				$fingerprint = $raw[0];
 				$password = $raw[1];
+				
+				if ($postpass == $password) {
+					echo("Login attempted by " . $fingerprint . " using password " . $password);
+					break;
+				}
 			}
 		}
 	}
