66b85608a1005a67c2b5c7f00a8d60d4da1cd5f3
redpois0n <red@cmail.nu>
2015-04-28 11:16:39 +0200

    running variable in config, reads each line in MAPFILE

--- login/config.php
+++ login/config.php
@@ -1,3 +1,11 @@
 <?php
 
+/**
+ * File where exit relays fingerprint and their unique passwords is
+ */
 const MAPFILE = "../map.txt";
+
+/**
+ * Disable this while running send.py through all exit nodes
+ */
+const RUNNING = true;
--- login/login.php
+++ login/login.php
@@ -2,7 +2,13 @@
 
 require_once "config.php";
 
-if (isset($_POST['username']) && isset($_POST['password'])) {
-	$login = $_POST['username'];
-	$pass = $_POST['password'];
+if (RUNNING) {
+	if (isset($_POST['username']) && isset($_POST['password'])) {
+		$login = $_POST['username'];
+		$pass = $_POST['password'];
+	
+		foreach (file(MAPFILE) as $line) {
+	
+		}
+	}
 }
